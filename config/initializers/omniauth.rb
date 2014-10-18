19d6ccd89704f211bc5d2859a89fa22a00437409
geh4y8 <guy.halperin88@gmail.com>
2014-10-17 20:44:07 -0700

    first commit mutha fuckas

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :khan_academy, 'M399NQr4R2tDncjW', 'jWSqFPCBpMhT9XN4'
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root to: "articles#index"
+
+  get "/auth/:provider/callback" => "sessions#create"
+  get "/signout" => "sessions#destroy", :as => :signout
+  resources :articles
+end
