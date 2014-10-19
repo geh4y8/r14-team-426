28cc3ba8b8a77b848810e178801cbfd2d7c1c06c
geh4y8 <guy.halperin88@gmail.com>
2014-10-18 23:55:41 -0700

    remove newbadge?

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,7 +5,6 @@ class ApiController < ApplicationController
     @user = User.find(params[:id])
     respond_to do | format |
       @user.get_badge_count(ApplicationController.get_access_token)
-      @user.new_badge = @user.new_badge?
       format.html
       format.json do
           render :json => {id: @user.id,
